5fed13b0b2e314fe5b8e2e6988837e62ef8912e2
fernandodossantos001 <fernandodossantos001@gmail.com>
2020-04-12 00:41:27 -0300

    relacionamento muitos para muitos

--- src/main/java/br/com/curso/mc/Application.java
+++ src/main/java/br/com/curso/mc/Application.java
@@ -1,13 +1,46 @@
 package br.com.curso.mc;
 
+import br.com.curso.mc.entity.Categoria;
+import br.com.curso.mc.entity.Produto;
+import br.com.curso.mc.repository.CategoriaRepository;
+import br.com.curso.mc.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
+import java.util.Arrays;
 
-    public static void main(String[] args) {
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria categoriainformatica= new Categoria("Informática" );
+        Categoria categoriaEscritorio = new Categoria("Escritório" );
+
+        Produto produtoComputador = new Produto("Computador", 2000.00);
+        Produto produtoImpressora = new Produto("Impressora", 800.00);
+        Produto produtoMouse = new Produto("Mouse", 80.00);
+
+        categoriainformatica.setProdutos(Arrays.asList(produtoComputador,produtoImpressora,produtoMouse));
+        categoriaEscritorio.setProdutos(Arrays.asList(produtoImpressora));
+
+        produtoComputador.setCategorias(Arrays.asList(categoriainformatica));
+        produtoImpressora.setCategorias(Arrays.asList(categoriainformatica,categoriaEscritorio));
+        produtoMouse.setCategorias(Arrays.asList(categoriainformatica));
+
+        categoriaRepository.saveAll(Arrays.asList(categoriainformatica,categoriaEscritorio));
+
+        produtoRepository.saveAll(Arrays.asList(produtoImpressora,produtoComputador,produtoMouse));
+
+    }
 }
---
+++ src/main/java/br/com/curso/mc/entity/Produto.java
@@ -0,0 +1,79 @@
+package br.com.curso.mc.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "t_produto")
+public class Produto implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private Double preco;
+    @ManyToMany
+    @JoinTable(name = "t_produto_categoria",
+            joinColumns = @JoinColumn(name = "id_produto"),
+            inverseJoinColumns = @JoinColumn(name = "id_categoria"))
+    private List<Categoria> categorias;
+
+    public Produto(String nome, Double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Produto(){
+        this.categorias = new ArrayList<Categoria>();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return id.equals(produto.id) &&
+                Objects.equals(nome, produto.nome) &&
+                Objects.equals(preco, produto.preco) &&
+                Objects.equals(categorias, produto.categorias);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, preco, categorias);
+    }
+}
