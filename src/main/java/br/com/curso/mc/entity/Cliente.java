de1e968f82a968198f735953f58e431241cda39a
Fernando Ribeiro <Developer@localhost.localdomain>
2020-05-10 22:25:48 -0300

    ajuste na classe cliente

--- src/main/java/br/com/curso/mc/entity/Cliente.java
+++ src/main/java/br/com/curso/mc/entity/Cliente.java
@@ -44,7 +44,7 @@ public class Cliente implements Serializable {
         this.nome = nome;
         this.email = email;
         this.cpnOuCnpj = cpnOuCnpj;
-        this.tipoCliente = tipoCliente.getCodigo();
+        this.tipoCliente = (tipoCliente==null) ? null: tipoCliente.getCodigo();
         enderecos = new ArrayList<Endereco>();
         telefones = new HashSet<String>();
     }
