a26f936d775666d64ef93cc345f3c60ec01ca69c
Fernando Ribeiro <Developer@localhost.localdomain>
2020-06-01 22:17:28 -0300

    criando profile de dev

---
+++ src/main/java/br/com/curso/mc/config/ProfileConfigDev.java
@@ -0,0 +1,26 @@
+package br.com.curso.mc.config;
+
+import br.com.curso.mc.PopulaBancoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class ProfileConfigDev {
+
+    @Autowired
+    private PopulaBancoService populaBancoService;
+    @Bean
+    public Boolean instantiateDataBase() throws ParseException {
+//        populaBancoService.createCategoriaAndProduto();
+//        populaBancoService.createEsdadoAndCidade();
+//        populaBancoService.createClienteAndEndereco();
+//        populaBancoService.createPedido();
+//        populaBancoService.createItemPedido();
+        return Boolean.TRUE;
+    }
+}
