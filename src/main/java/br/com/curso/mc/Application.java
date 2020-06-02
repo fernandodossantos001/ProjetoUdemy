5f7c625621d25722df6ae72ea629b62a8e0fc7f6
Fernando Ribeiro <Developer@localhost.localdomain>
2020-06-02 20:45:43 -0300

    retirando a interface CommanRunner

--- src/main/java/br/com/curso/mc/Application.java
+++ src/main/java/br/com/curso/mc/Application.java
@@ -1,26 +1,12 @@
 package br.com.curso.mc;
 
-import br.com.curso.mc.entity.Categoria;
-import br.com.curso.mc.entity.Cidade;
-import br.com.curso.mc.entity.Estado;
-import br.com.curso.mc.entity.Produto;
-import br.com.curso.mc.repository.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application{
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
     }
 }
